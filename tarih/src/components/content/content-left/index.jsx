43f33e86b9415c2822c6526319a2a745638391ee
Adil <adilzhapar2002@gmail.com>
2022-07-12 15:39:06 +0600

    upd

--- tarih/src/components/content/content-left/index.jsx
+++ tarih/src/components/content/content-left/index.jsx
@@ -1,3 +1,17 @@
+import events from "../../../data/left.json";
+
+
 export const Left = () => {
-  return <div>Left</div>;
+  events.map((item) => (
+    console.log(item.img)
+  ))
+  
+  return <div>
+    {
+      events.map((item) => (
+        <img key={item.key} src={item.img} alt="item" />
+      ))
+
+    }
+  </div>;
 };
