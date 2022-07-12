2d154d89558386fe242893c44f1e20bfd4857d92
Adil <adilzhapar2002@gmail.com>
2022-07-13 03:36:15 +0600

    upd

--- tarih/src/components/content/content-left/index.jsx
+++ tarih/src/components/content/content-left/index.jsx
@@ -1,20 +1,37 @@
-import events from "../../../data/left.json";
+import data from "../../../data/kaz.json";
 
 export const Left = () => {
   return (
     <div className="left">
-      {events.map((item) => (
-        <div className="left-item">
-          <div className="left-item-text">
-            {item.text.map((txt) => (
-              <p>{txt}</p>
-            ))}
-          </div>
+      {data.map((item) => (
+        <div key={item.key} className="left-item">
           <div className="left-item-img">
-            <img key={item.key} src={item.img} alt="item" />
+            {item.imgs.map((image) => (
+              <img src={image} alt=""/>
+            ))
+            }
+          </div>
+          <div className="left-item-events">
+            {item.events.map((event) => (
+              <p>{event}</p>
+            ))
+            }
+          </div>
+          <div className="left-item-innovations">
+            {item.innovations.map((inv) => (
+              <p>{inv}</p>
+            ))
+            }
+          </div>
+          <div className="left-item-famous">
+            {item.famous.map((fms) => (
+              <p>{fms}</p>
+            ))
+            }
           </div>
         </div>
-      ))}
+      ))
+      }
     </div>
   );
 };
--- tarih/src/components/content/content-right/index.jsx
+++ tarih/src/components/content/content-right/index.jsx
@@ -1,22 +1,38 @@
-import events from "../../../data/left.json";
+import data from "../../../data/world.json";
 
 export const Right = () => {
   return (
     <div className="right">
-      {events.map((item) => (
-        <div className="right-item">
+      {data.map((item) => (
+        <div key={item.key} className="right-item">
           <div className="right-item-img">
-            <img key={item.key} src={item.img} alt="item" />
+            {item.imgs.map((image) => (
+              <img src={image} alt=""/>
+            ))
+            }
           </div>
-          <div className="right-item-text">
-            {item.text.map((txt) => (
-              <ul>
-                <li>{txt}</li>
-              </ul>
-            ))}
+          <div className="right-item-events">
+            {item.events.map((event) => (
+              <p>{event}</p>
+            ))
+            }
+          </div>
+          <div className="right-item-innovations">
+            {item.innovations.map((inv) => (
+              <p>{inv}</p>
+            ))
+            }
+          </div>
+          <div className="right-item-famous">
+            {item.famous.map((fms) => (
+              <p>{fms}</p>
+            ))
+            }
           </div>
         </div>
-      ))}
+      ))
+      }
     </div>
+
   );
 };
